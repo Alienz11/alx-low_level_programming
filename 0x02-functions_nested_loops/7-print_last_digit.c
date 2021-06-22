f4f9f7a356b4507945c8b09440e7ca95cba7495f
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-22 15:31:38 +0100

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Alx school students.
+ * @c: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int c);
+
+{
+l_d = c % 10;
+if (l_d >= 0)
+{
+_putchar(l_d);
+return (l_d);
+}
+else
+{
+_putchar(l_d * -1);
+return (l_d * -1);
+}
+}
