64c834410cb9e2ad58335f980324ac588e7eeff8
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-22 12:52:19 +0100

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - check the code for Alx school students.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void);
+
+{
+int i;
+for (i = 0; i <= 9, i++)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -9,5 +9,6 @@
 
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif /* _SAMPLE_HEADER_H_ */
