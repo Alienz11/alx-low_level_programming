fe29a54cd31ba1944c13385c3c4bfcd67361e8bc
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-22 18:42:17 +0100

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * add - check the code for Alx school students.
+ *@x: argument of function.
+ *@y: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+int c add(int x, int y)
+
+{
+return (x + y);
+}
