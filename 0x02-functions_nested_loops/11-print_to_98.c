54903defa9f3835b2cb4fb6f691240547a4fea30
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-22 20:08:22 +0100

    Holberton School, 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * print_to_98 - check the code for Alx school students.
+ * @n: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+
+{
+for (n = 0; n <= 98, n++)
+{
+_putchar(n);
+if ('n' <= 97)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+return (0);
+}
