0823059ec7769139fb429e762a046a937e4486c5
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-22 13:14:11 +0100

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,12 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
+
 {
 int i;
 char c;
- 
+
 for (i = 0; i <= 9, i++)
 {
 for (c = 'a'; c <= 'z'; c++)
