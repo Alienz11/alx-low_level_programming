a20c17b2871f592fc23a1c190655656fbd27a30d
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-22 13:43:17 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * _islower - check the code for Alx school students.
+ * @c: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+
+{
+unsigned char c;
+if (_islower(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
