7c07bb83eb43bdf35c0ffa432db04809f621ee96
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-22 14:22:51 +0100

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * print_sign - check the code for Alx school students.
+ * @n: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+
+{
+if (n > 0)
+{
+return (1);
+_putchar('+');
+}
+else if (n == 0)
+{
+return (0);
+_putchar('0');
+}
+else
+{
+return (-1);
+_putchar(-1);
+}
+}
