4e35fb4bddecc8f79cb1e3fd5e65c9935e7368c4
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-24 16:24:47 +0100

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -13,6 +13,8 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void _putchar(void);
+int _putchar(char);
+
+
 
 #endif /* _SAMPLE_HEADER_H_ */
