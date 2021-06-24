35865faf406f24d9d662dda67daa166db70208ee
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-24 19:27:44 +0100

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * main - check the code for Alx school students.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int i;
+
+for (i = 0; i <= 100, 1++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+return (0);
+}
