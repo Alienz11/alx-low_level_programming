7b77776cc27e55a0ab1b0a02f271d791d72d0cae
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-24 14:55:55 +0100

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "holberton.h"
 
 /**
- * _isupper - check the code for Alx school students.
- * @c: argument of function.
- * Return: Always 0 (Success)
+ * _isupper - check the code for uppercase
+ *@c: parameter the int will use to check for the argument of the function
+ * Return: Always 0.
  */
-
 int _isupper(int c)
-
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= 'A' &&  c <= 'Z')
 {
 return (1);
 }
