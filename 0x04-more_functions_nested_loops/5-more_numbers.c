c52e8667930244696f1b52d3c401ec1b04d8cbbe
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-24 16:19:52 +0100

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * more_numbers - check the code for Alx school students.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+
+{
+int n;
+for (n = 0; n <= 10; n++)
+{
+for (n = '0'; n <= '14'; n++)
+{
+_putchar(n + '0');
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -12,6 +12,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-
+void more_numbers(void);
+void _putchar(void);
 
 #endif /* _SAMPLE_HEADER_H_ */
