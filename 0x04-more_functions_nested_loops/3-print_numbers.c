1796f151b437730bb4f3661f6a0657df628d494e
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-24 15:34:09 +0100

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,13 @@
 
 /**
  * print_numbers - check the code for Alx school students.
- *@n: parameter the int will use to check for the argument of the function
+ *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
 int n;
-for (n = '0'; n <= '9', n++)
+for (n = '0'; n <= '9'; n++)
 {
 _putchar(n);
 }
