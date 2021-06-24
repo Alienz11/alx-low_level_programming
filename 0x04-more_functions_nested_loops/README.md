75b6d3782fafdc77b886c41997e6c18345ca1e49
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-24 14:43:12 +0100

    isupper

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+**This folder/directory displays all the under-pinnings of advanced function prototypes and nested loops in c programming language**
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,11 @@
+#ifndef Holberton H
+#define Holberton H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _isupper(int c);
+
+#endif /*Holberton H*/
