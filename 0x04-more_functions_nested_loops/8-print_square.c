3bcb5682ee3ba58b421f4219bf8b5142d0c05efc
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-24 18:11:03 +0100

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * print_square - a function that draws a square with # sign on the terminal
+ * @size: input number of times '\' should be printed
+ * Return: a diagonal
+ */
+
+void print_square(int size);
+{
+int c, v;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (c = 1; c <= size; c++)
+{
+for (v = 1; v == c; v++)
+{
+_putchar('#');
+}
+_putchar('#');
+_putchar('\n');
+}
+}
+}
