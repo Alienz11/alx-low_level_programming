fecfd314c740e3f1da92becc81823e39c2264466
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-24 15:27:35 +0100

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_numbers - check the code for Alx school students.
+ *@n: parameter the int will use to check for the argument of the function
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int n;
+ for (n = 0; n <=9, n++)
+{
+_putchar (n);
+}
+_putchar ('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -10,6 +10,8 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+void _putchar(void);
 
 
 
