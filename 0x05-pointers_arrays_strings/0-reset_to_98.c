7328d08465bf88e441a00c347f6c4c30b5a6f8eb
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-28 12:04:33 +0100

    firt commit,98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,19 +5,12 @@
 
 /**
  * reset_to_98 - check the code for Alx school students.
- * @n: argument of function.
+ * @n: Integer argument for dereferencing a variable
  *
  * Return: Always 0 (Success)
  */
 
 void reset_to_98(int *n)
-
 {
-int i;
-
-i = 402;
-n = &i;
-printf("n=%d\n", i);
 *n = 98;
-printf("n=%d\n", i);
 }
