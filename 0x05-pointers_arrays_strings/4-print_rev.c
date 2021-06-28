6888b4e79bb67829dfe1de2f4de1ca998cdf14f1
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-28 15:06:47 +0100

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * print_rev - check the code for Alx school students.
+ * @s: argument of function.
+ * Return: void
+ */
+
+void print_rev(char *s)
+
+{
+int i;
+for (i = 0; str[i] != 0; --i)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
