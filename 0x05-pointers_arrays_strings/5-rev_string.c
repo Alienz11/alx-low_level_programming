193490ca2e867e1d3af3e5ce95e30e9506c79732
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-06-28 16:10:23 +0100

    A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * rev_string - check the code for Alx school students.
+ * @s: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+
+{
+int i;
+for (i = 0; s[i] != 0; i++)
+{
+}
+for (i = i - 1; i >= 0; i--)
+{
+}
+}
