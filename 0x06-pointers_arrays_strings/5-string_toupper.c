042b162d009d991084b5faa93669c59eeb706243
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-01 15:45:32 +0100

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * string_toupper - check the code for Alx school students.
+ *
+ * @str: Name of array
+ * Return: Always 0 (Success)
+ */
+
+char *string_toupper(char *str)
+
+{
+int c;
+for (c = 0; str[c] != '\0'; c++)
+{
+if (str[c] >= 'a' && str[c] <= 'z')
+{
+str[c] = str[c] - 32;
+}
+}
+return (str);
+}
