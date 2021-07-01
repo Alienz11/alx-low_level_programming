63e29b5c6a95088837f4967ce820a3af05b3a491
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-01 18:42:30 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * string_toupper - check the code for Alx school students.
+ *
+ * @str: Name of array
+ * Return: Always 0 (Success)
+ */
+
+char *string_toupper(char *str)
+
+{
+int c, v;
+char sym[] = " \t\n,;.!?\"(){}";
+
+c = 1;
+if (str[0] >= 'a' && str[0] <= 'z')
+str[0] -= ('a' - 'A');
+while (str[c] != '\0')
+{
+for (v = 0; sym[v] != '\0'; v++)
+if (str[c - 1] == sym[v] && (str[c] >= 'a' && str[c] <= 'z'))
+str[c] -= ('a' - 'A');
+c++;
+}
+return (str);
+}
