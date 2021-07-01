51356bca08a3ec53f141b485edae87d4b82f6ad0
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-01 07:50:16 +0100

    Third commit, Strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * _strncpy - check the code for Alx school students.
+ * @n: argument of function.
+ * @src: argument of function.
+ * @dest: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+int i = -1;
+
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
