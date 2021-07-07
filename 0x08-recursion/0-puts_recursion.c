ac7ed835caa67e00120549c4de0c5165cc97e4ec
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-07 11:25:14 +0100

    My first commit,She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Using recursion to print out strings.
+ * @s: character argument of function.
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{  
+return;
+}
+else
+{
+_putchar(*s);
+}
+_puts_recursion(s + 1);
+}
