8058c8176057f9ec3ed3122748f5e0023896dc16
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-07 12:51:57 +0100

    Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Using recursion to print out strings.
+ * @s: character argument of function.
+ * Return: Always 0 (Success)
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s - 1);
+_print_rev_recursion((s - 1) - 1);
+}
+}
