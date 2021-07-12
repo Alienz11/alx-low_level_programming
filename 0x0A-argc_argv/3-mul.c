b0001f9cb244d710cecbdd6f50353fe72d624c82
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-12 16:38:51 +0100

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * main - To creat a function that names the file even when renamed.
+ * @argc: argument of function.
+ * @argv: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+i = atoi(argv[1]) * atoi(argv[2]);
+if (argc == 3)
+{
+printf("%d\n", i);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
