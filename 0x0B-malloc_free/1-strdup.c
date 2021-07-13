91b76b3e5eb7131b3dd03d6e66956b5baf547e77
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-13 16:08:18 +0100

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * _strdup - check the code for Alx school students.
+ * @str: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+char *_strdup(char *str)
+{
+
+unsigned int len = 0;
+unsigned int i = 0;
+char *array;
+
+while (str[i] != '\0')
+{
+len++;
+i++;
+}
+array = malloc(len * (sizeof(char)));
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str == NULL)
+{
+return (NULL);
+}
+array[i] = str[i];
+}
+return (array);
+}
