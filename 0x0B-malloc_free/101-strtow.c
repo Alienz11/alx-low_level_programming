f1aaf080e14b98f75a2fd5bd3a94e67ead42caf7
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-13 18:11:05 +0100

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,17 +12,18 @@ char **strtow(char *str);
  *
  * Return: The index marking the end of the initial word pointed to by str.
  */
+
 int word_len(char *str)
 {
-  int index = 0, len = 0;
+int index = 0, len = 0;
 
-  while (*(str + index) && *(str + index) != ' ')
-    {
-      len++;
-      index++;
-    }
+while (*(str + index) && *(str + index) != ' ')
+{
+len++;
+index++;
+}
 
-  return (len);
+return (len);
 }
 
 /**
@@ -33,21 +34,21 @@ int word_len(char *str)
  */
 int count_words(char *str)
 {
-  int index = 0, words = 0, len = 0;
+int index = 0, words = 0, len = 0;
 
-  for (index = 0; *(str + index); index++)
-    len++;
+for (index = 0; *(str + index); index++)
+len++;
 
-  for (index = 0; index < len; index++)
-    {
-      if (*(str + index) != ' ')
-	{
-	  words++;
-	  index += word_len(str + index);
-	}
-    }
+for (index = 0; index < len; index++)
+{
+if (*(str + index) != ' ')
+{
+words++;
+index += word_len(str + index);
+}
+}
 
-  return (words);
+return (words);
 }
 
 /**
