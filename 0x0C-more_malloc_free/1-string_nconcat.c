97b3ec346b286e609e0c89c41d0e10aa70b295c9
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-16 00:57:26 +0100

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,40 +11,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int i, j, len1 = n;
+unsigned int i, j, len1 = n;
 unsigned int len2 = 0;
-char *s3, empty;
-empty = "";
+char *s3;
+
 if (s1 == NULL)
-{
-s1 = empty;
-}
+s1 = "";
 if (s2 == NULL)
-{
-s2 = empty;
-}
+s2 = "";
 for (i = 0; s1[i] != '\0'; i++)
-{
 len1++;
-}
 for (j = 0; s2[j] != '\0'; j++)
-{
 len2++;
-}
 s3 = malloc(sizeof(char) * (len1 + 1));
 if (s3 == NULL)
-{
 return (NULL);
-}
 len1 = 0;
-for (i = 0; s1[i] != '\0'; i++)
-{
+for (i = 0; s1[i]; i++)
 s3[len1++] = s1[i];
-}
 for (j = 0; s2[j] && i < n; j++)
-{
 s3[len1++] = s2[j];
 s3[len1] = '\0';
-}
 return (s3);
 }
