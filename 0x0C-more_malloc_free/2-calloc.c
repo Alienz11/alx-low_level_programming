4afb14817354c2d6f9c92dd7648e6265856579b2
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-15 18:46:51 +0100

    string_nconcat

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * _calloc - check the code for Alx school students.
+ * @nmemb: argument of function.
+ * @size: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *str;
+unsigned int i = 0;
+char *set;
+
+if (size == 0)
+{
+return (NULL);
+}
+if (nmemb == 0)
+{
+return (NULL);
+}
+str = malloc(size * nmemb);
+set = str;
+for (i = 0; i < (size * nmemb); i++)
+{
+set[i] = 0;
+}
+if (str == NULL)
+{
+return (NULL);
+}
+return (str);
+}
