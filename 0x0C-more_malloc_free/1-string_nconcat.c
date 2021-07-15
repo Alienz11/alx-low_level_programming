959d919c5fc7ed1a9f17c5bbe9b750a1378768f1
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-15 17:21:27 +0100

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include "holberton.h"
-
 /**
  * string_nconcat - check the code for Alx school students.
  * @n: argument of function.
@@ -10,12 +9,10 @@
  * @s2: argument of function.
  * Return: Always 0 (Success)
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int len1, len2, i, j;
 char *empty, *s3, *s4;
-
 empty = "";
 if (s1 == NULL)
 {
@@ -34,30 +31,23 @@ while (s2[len2] != '\0')
 {
 len2++;
 }
-
 if (n >= len2)
 {
-s4 = malloc((len2) * (sizeof(n)));
+s4 = malloc((len2)*(n));
 return (s4);
 }
-
-s3 = malloc((len1+len2) * (sizeof(n)));
+s3 = malloc((len1 + len2)*(n));
 if (s3 == NULL)
 {
 return (NULL);
 }
-i = 0;
-while (i < len1)
+for (i = 0; i < len1; i++)
 {
 s3[i] = s1[i];
-i++;
 }
-j = 0;
-while (j <= len2)
+for (j = 0; j <= len2; i++, j++)
 {
 s3[i] = s2[j];
-i++;
-j++;
 }
 s3[i] = '\0';
 return (s3);
