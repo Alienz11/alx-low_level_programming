5c2674fd45f0b036e3a619aa4e9d09df74b96c7c
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-16 21:21:41 +0100

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,35 +2,53 @@
 #include <stdlib.h>
 #include <time.h>
 #include "holberton.h"
+
 /**
- * string_nconcat - check the code for Alx school students.
- * @n: argument of function.
- * @s1: argument of function.
- * @s2: argument of function.
- * Return: Always 0 (Success)
+ * _strlen - return length of string
+ *
+ * @str: string argument
+ *
+ * Return: int
  */
+
+
+int _strlen(char *str)
+{
+int counter = 0;
+
+while (*str != 0)
+{
+counter++;
+str++;
+}
+return (counter);
+}
+
+
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: pointer to new space in memory that contains s1 + n bytes of s2
+ */
+
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, len1 = n;
-unsigned int len2 = 0;
-char *s3;
-
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (i = 0; s1[i] != '\0'; i++)
-len1++;
-for (j = 0; s2[j] != '\0'; j++)
-len2++;
-s3 = malloc(sizeof(char) * (len1 + 1));
-if (s3 == NULL)
+unsigned int lens1, lens2, nofbyt, i, j;
+char *p;
+lens1 = (s1 != NULL) ? _strlen(s1) : 0;
+lens2 = (s2 != NULL) ? _strlen(s2) : 0;
+nofbyt = (n >= lens2) ? lens2 : n;
+p = malloc((lens1 + nofbyt + 1) * sizeof(char));
+if (p == NULL)
 return (NULL);
-len1 = 0;
-for (i = 0; s1[i]; i++)
-s3[len1++] = s1[i];
-for (j = 0; s2[j] && i < n; j++)
-s3[len1++] = s2[j];
-s3[len1] = '\0';
-return (s3);
+for (i = 0; i < lens1; i++)
+p[i] = s1[i];
+for (j = 0; j < nofbyt; j++)
+p[i + j] = s2[j];
+p[i + j] = '\0';
+return (p);
 }
