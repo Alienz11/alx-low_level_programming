766234ddd3370be9efab3c8303528488069dbd58
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-23 01:06:23 +0100

    To be is a to be the value of a variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,25 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
 
-/*
- * Structs, enums and unions definitions
- * Typedefs
- * Function prototypes
- */
 
-int _putchar(char);
 #include <string.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct print - A struct fuction taht gives details of a dog.
+ * @format: vriable to print name of dog.
+ * @arg: variable to print age of dog.
+ *
+ */
+
+typedef struct print
+{
+char *view;
+void (*share)(va_list arg);
+}print_t;
 
 #endif /*_variadic_functions_h_*/
