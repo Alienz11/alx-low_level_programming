943091155f6f07c80008d4fa72db1754c7faf2f5
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-29 23:42:28 +0100

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -13,6 +13,7 @@ void list_first(void)__attribute__((constructor));
 void list_first(void)
 
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,
+\nI bore my house upon my back!\n");
 
 }
