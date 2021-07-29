45d4367ab3f97455ba67245a6374515e3de80dac
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-29 21:48:06 +0100

    First commit, Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
 /**
  * print_list - A stringlist funtion to print a list.
  * @h: Argument variable of the function.
- *
+ * Return: Amount of nodes counted
  */
 
 size_t print_list(const list_t *h)
