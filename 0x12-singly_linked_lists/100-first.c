cc59167815a9cda895d57d25871a6753d31f5a4a
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-07-29 23:41:44 +0100

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void list_first(void)__attribute__((constructor));
+
+/**
+ * list_first - check the code for Alx school students.
+ *
+ *
+ */
+
+void list_first(void)
+
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+}
