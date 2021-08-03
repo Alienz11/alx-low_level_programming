3c71a271cf40031c79ccd1c8adb4dc3419db25a4
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-08-03 22:30:28 +0100

    Solutions to Advance

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,14 +1,25 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
+#define PRINT_LOOP_NODE(node) {\
+print_int_str(0, "[", 0); \
+print_ptr((void *)(node), 0); \
+print_int_str(0, "] ", 0); \
+print_int_str((node)->n, NULL, 0);\
+_putchar('\n'); }
+#define PRINT_LOOP_NODE_2(node) {\
+print_int_str(0, "-> [", 0); \
+print_ptr((void *)(node), 0); \
+print_int_str(0, "] ", 0); \
+print_int_str((node)->n, NULL, 0);\
+_putchar('\n'); }
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- *
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -16,12 +27,13 @@ int n;
 struct listint_s *next;
 } listint_t;
 
-int _putchar(char);
 size_t print_listint(const listint_t *h);
+int _putchar(char c);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+void free_listint2_head(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
@@ -32,6 +44,12 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char exists(void **arr, size_t n, void *item);
+void print_ptr(void *ptr, char stage);
+void print_int_str(int num, char *str, char stage);
+void *_realloc1(void *ptr, unsigned int old_size, unsigned int new_size);
+char exists1(void **arr, size_t n, void *item);
+void free_nodes(void **nodes_ptr, size_t n);
 
-
-#endif /*_LISTS_H_*/
+#endif
