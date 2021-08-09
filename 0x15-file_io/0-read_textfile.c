1b8e6f24645c147ed297fbfdcc3494ab52de0681
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-08-09 15:51:53 +0100

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,8 +15,35 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+int fd;
+ssize_t readfun, writefun;
+char *buffer;
 
+if (filename == NULL)
+return (0);
 
+fd = open(filename, O_RDWR);
+if (fd == -1)
+return (0);
 
+buffer = malloc(sizeof(char) * letters);
+if (!buffer)
+{
+free(buffer);
+return (0);
+}
 
+readfun = read(fd, buffer, letters);
+
+if (readfun == -1)
+return (0);
+
+writefun = read(STDOUT_FILENO, buffer, readfun);
+if (readfun == -1)
+return (0);
+
+free (buffer);
+close (fd);
+
+return (writefun);
 }
