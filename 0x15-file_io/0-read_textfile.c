d3603175fdc03b2bbc5d4491e58416a8c66a2a1d
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-08-09 13:24:03 +0100

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * read_textfile - A function that creates a file that reads text.
+ * @filename: argument of function.
+ * @letters: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+
+
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+**This folder/directory displays all the under-pinnings of File Descriptors in C programming language**
