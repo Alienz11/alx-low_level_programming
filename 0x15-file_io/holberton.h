ff3fb7e6d1550dde0b73d48a525a7ab13ea7a292
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-08-10 21:42:03 +0100

    Update

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,8 +8,29 @@
  */
 int _putchar(char);
 #include <string.h>
+#include <stdlib.h>
+#include <elf.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char);
+#include <string.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#
+
+void ensure_endianness(char little_e, void *data, unsigned int len);
+void print_elf_header(void *h);
+void print_elf_header_2(void *h);
+void put_err(char *str);
 
 #endif /*_HOLBERTON_H_*/
+
+#ifndef HEADERS_H
+#define HEADERS_H
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#undef HEADERS_H
+#endif /* HEADERS_H */
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,19 @@ int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+#include <stdlib.h>
+#include <elf.h>
 
+void ensure_endianness(char little_e, void *data, unsigned int len);
+void print_elf_header(void *h);
+void print_elf_header_2(void *h);
+void put_err(char *str);
 #endif /*_MAIN_H_*/
+
+#ifndef HEADERS_H
+#define HEADERS_H
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#undef HEADERS_H
+#endif /* HEADERS_H */
