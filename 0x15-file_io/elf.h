2f3557aa4d440bf76c47dfdd9a49685e7c982ecd
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-08-10 21:31:33 +0100

    Update

--- 0x15-file_io/elf.h
+++ 0x15-file_io/elf.h
@@ -1,8 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <elf.h>
+
+void ensure_endianness(char little_e, void *data, unsigned int len);
+void print_elf_header(void *h);
+void print_elf_header_2(void *h);
+void put_err(char *str);
+
+#endif /* MAIN_H */
+
 #ifndef HEADERS_H
 #define HEADERS_H
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #undef HEADERS_H
-
 #endif /* HEADERS_H */
