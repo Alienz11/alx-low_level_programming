4ced04a7d5cf07bcb30c219ce74cf50592afefbd
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-09-16 18:18:45 +0100

    A fuction hat adds a new node to the beginning of the double linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * *add_dnodeint - adds a node to the beginning of a double linked list
+ * @head: linked list head
+ * @n: int element of the linked list
+ * Return: number of elements
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new_node;
+
+new_node = malloc(sizeof(dlistint_t));
+
+if (new_node == NULL)
+{
+free(new_node);
+return (NULL);
+}
+
+new_node->n = n;
+new_node->next = *head;
+new_node->prev = NULL;
+*head = new_node;
+
+return (new_node);
+}
