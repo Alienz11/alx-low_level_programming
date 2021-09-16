93bbfb5f544ab9c7a27293232dd9cbcbca688c06
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-09-16 23:07:11 +0100

    A function that puts a node in a desired position of a doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,43 +4,45 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at specific index
+ * insert_dnodeint_at_index - a function that inserts a
+ * new node at a given position.
+ * @idx: the index of the list where the new node should
+ * be added. Index starts at 0
+ * @n: input node
  * @h: head of linked list
- * @idx: index of new node
- * @n: new node value
- * Return: inserted node
+ *
+ * Return:  the address of the new node, or NULL if it failed
+ * if it is not possible to add the new node at index idx, do not
+ * add the new node and return NULL
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *present;
-dlistint_t *new_node;
-
-if (h == NULL)
-return (0);
-
-present = *h;
-
-while (idx != 0)
+dlistint_t *tmp = *h, *new;
+if (idx == 0)
 {
-present = present->next;
-idx--;
-if (present == NULL)
+return (add_dnodeint(h, n));
+}
+for (; idx != 1; idx--)
+{
+tmp = tmp->next;
+if (!tmp)
+{
 return (NULL);
 }
-new_node = malloc(sizeof(dlistint_t));
-
-if (new_node == NULL)
+}
+if (!tmp->next)
 {
-free(new_node);
+return (add_dnodeint_end(h, n));
+}
+new = malloc(sizeof(dlistint_t));
+if (!new)
+{
 return (NULL);
 }
-
-new_node->n = n;
-new_node->next = present;
-new_node->prev = present->prev;
-if (present->prev != NULL)
-present->prev->next = new_node;
-
-return (present);
+new->n = n;
+new->prev = tmp;
+new->next = tmp->next;
+tmp->next->prev = new;
+tmp->next = new;
+return (new);
 }
