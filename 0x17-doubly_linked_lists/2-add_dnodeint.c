0f01e1ebb645b574a1a9474bf967ee1bb28b8c98
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-09-16 18:49:29 +0100

    A fuction hat adds a new node to the beginning of the double linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,17 +16,10 @@ dlistint_t *new_node;
 
 new_node = malloc(sizeof(dlistint_t));
 
-if (new_node == NULL)
-{
-free(new_node);
-return (NULL);
-}
-
 new_node->n = n;
 new_node->next = NULL;
 new_node->prev = NULL;
 new_node->next = *head;
-*head->prev = new_node;
 *head = new_node;
 
 return (new_node);
