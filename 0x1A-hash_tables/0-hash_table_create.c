07d5ead0635fa43a1e8a7d3710b579e6914d0cf3
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-10-08 12:22:17 +0100

    Test commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create - A function tht creates a harsh table.
+ * @size: The arguments that decides the size of a harsh table.
+ * Return: a pointer to the newly created harsh table (table).
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+/* Creates a new HashTable*/
+hash_table_t *table;
+
+if (size <= 0)
+return (NULL);
+
+if (size)
+{
+table = (hash_table_t *)malloc(size * sizeof(unsigned long int));
+table->size = size;
+}
+
+return (table);
+}
