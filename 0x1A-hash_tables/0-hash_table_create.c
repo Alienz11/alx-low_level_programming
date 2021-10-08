42e0fcf3fefed1a16e30b9b1a361131dbd5ebdbb
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2021-10-08 13:09:46 +0100

    A function that creates a harsh table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,18 +12,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 /* Creates a new HashTable*/
 hash_table_t *table;
 
-if (size <= 0)
+if (size < 1)
 return (NULL);
 
 table = (hash_table_t *)malloc(size * sizeof(hash_table_t));
 if (table == NULL)
 return (NULL);
-
-table->array = malloc(size * sizeof(hash_table_t));
-if (table->array == NULL)
-return (NULL);
-
-table->size = size;
+/*
+ * table->array = malloc(size * sizeof(hash_table_t));
+ * if (table->array == NULL)
+ * return (NULL);
+*/
 
 return (table);
 }
